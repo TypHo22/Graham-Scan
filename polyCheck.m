9c4588798cb70758d01479fa67baab1b957cf2ea
TypHo22 <42981587+TypHo22@users.noreply.github.com>
2019-08-20 22:42:31 +0200

    Update polyCheck.m

--- polyCheck.m
+++ polyCheck.m
@@ -1,28 +1,28 @@
 function [polyKruemmung] = polyCheck(P1,P2,P3)
-%Definition:  Eine Ecke pi in einem Polygonzug p0p1,  p1p2, ...,  
-% pn-1p0 heißt konvex, wenn für den links liegenden Winkel ? zwischen den Kanten
-% pi-1pi und pipi+1 gilt 0° kleiner gleich ? < 180° (i-1 und i+1 werden modulo n gerechnet). 
-% Anderenfalls heißt die Ecke konkav.
-% konkav = rechtsdrehung
-% konvex = linksdrehung
-% Ich habe mich dafuer entschieden ueber die Dreiecksflaeche zu bestimmen,
-% ob der Polygonzug konkav oder konvex ist. Hierzu bedient man sich dem
-% Kreuzprodukt. Wenn die Werte des Kreuzprodukts addiert einen negativen Wert
-% fuer polyKruemmung ergeben, dann ist der Polygonzug konkav. Bei einem positiven Wert fuer polyKruemmung ergibt sich
-% ein konvexer Polygonzug
-% PolyVal < 0 = konkav = rechtsdrehung
-% PolyVal > 0 = konvex = linksdrehung
-% Anstelle von linksdrehung oder rechtsdrehung kann auch von einer
-% kruemmnung gesprochen werden
+%Definition: One corner pi in a polygon course p0p1, p1p2, ...,  
+% pn-1p0 means convex, if for the left angle ? between the edges
+% pi-1pi and pipi+1 is 0Â° less than or equal to ? < 180Â° (i-1 and i+1 are calculated modulo n). 
+%Otherwise the corner is called concave.
+% concave = clockwise rotation
+% convex = counterclockwise rotation
+% I have chosen to determine the cross product,
+% whether the polygon course is concave or convex.  If the sum of the cross product does have a negative value
+%  then the polygon course is concave. With a positive value for polycrumming the result is
+% a convex polygon course
+% PolyVal < 0 = concave = clockwise rotation
+% PolyVal > 0 = convex = left rotation
+% You could also check for convex or concav via the distances P0P1 and P0P2
+% Instead of turning to the left or to the right, it is also possible to use a
+% curvature will be spoken
 %% Andreas Bernatzky 19.08.2019
 %%
 % f = [P1;P2;P3];
 % figure(2);
 % scatter(f(:,1),f(:,2),'filled');
-P1P2 = sqrt((P2(1)-P1(1))^2 + (P2(2)-P1(2))^2 );%laenge der strecke
-P1P3 = sqrt((P3(1)-P1(1))^2 + (P3(2)-P1(2))^2 );%laenge der strecke
-P1P2Vec = [(P2(1)-P1(1));(P2(2)-P1(2));0]; %vektor aufstellen spitze minus fuss
-P1P3Vec = [(P3(1)-P1(1));(P3(2)-P1(2));0]; % vektor aufstellen spitze minus fuss
+P1P2 = sqrt((P2(1)-P1(1))^2 + (P2(2)-P1(2))^2 );%length of the route
+P1P3 = sqrt((P3(1)-P1(1))^2 + (P3(2)-P1(2))^2 );%length of the route
+P1P2Vec = [(P2(1)-P1(1));(P2(2)-P1(2));0]; %vector set up tip minus foot
+P1P3Vec = [(P3(1)-P1(1));(P3(2)-P1(2));0]; %vector set up tip minus foot
 %% P1P2Vec P1P3Vec
 crossP = cross(P1P2Vec,P1P3Vec);
 polyKruemmung = crossP(1)+crossP(2)+crossP(3);
